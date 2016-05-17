09f55c9cec420922716acb227b47382c278a1113
wangyang06 <wangyang06@xywy.com>
2016-05-17 16:00:38 +0800

    first

---
+++ IM/app/src/main/java/com/huanxin/im/MainActivity.java
@@ -0,0 +1,17 @@
+package com.huanxin.im;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hyphenate.chatuidemo.ui.SplashActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(this, com.hyphenate.chatuidemo.ui.SplashActivity.class);
+        startActivity(i);
+    }
+}
